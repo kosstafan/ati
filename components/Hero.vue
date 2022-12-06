1e43fd9c7454b9eff09494a7e4cd3c32259218df
Konstantin G <kostafan_roc@list.ru>
2022-12-06 14:11:19 +0500

    расчёт vh

--- components/Hero.vue
+++ components/Hero.vue
@@ -38,6 +38,11 @@ export default {
     data: () => ({
         feachers: ['по всей России','Белоруссия','Грузия','Казахстан','Китай','Таджикистан','Узбекистан'],
     }),
+    mounted() {
+        let vh = window.innerHeight * 0.01;
+        // Then we set the value in the --vh custom property to the root of the document
+        document.documentElement.style.setProperty('--vh', `${vh}px`);
+    },
     methods: {
     scrollToBottom() {
       window.scrollTo({
